a6c6a5723ad4cbc3dd573537ac1d8b784a137504
Lucas Silverio <lucas.silveriosousa@gmail.com>
2023-08-06 21:27:31 -0300

    Alteração no heaader, incluindo o link do meu perfil

--- resources/views/layouts/_partials/header.blade.php
+++ resources/views/layouts/_partials/header.blade.php
@@ -2,6 +2,6 @@
 <header class="w-full items-center bg-white py-2 px-6 hidden sm:flex">
     <div class="w-1/2"></div>
     <div x-data="{ isOpen: false }" class="relative w-1/2 flex justify-end">
-        <i>By @LucasSilverio</i>
+       <a href="https://www.linkedin.com/in/lucasilverio/" target="_blank">Desenvolvido por Lucas Silvério</a>
     </div>
 </header>
